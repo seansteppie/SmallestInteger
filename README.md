de0712536962eb23664f9ae113e269ca8f77b8d1
Sean Steppie <sean.steppie@steel-eyed.com>
2021-04-07 13:47:42 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+* com.steel_eyed.SmallestIntegerFinder Wed  7 Apr 13:47:42 BST 2021
---
+++ src/main/java/com/steel_eyed/SmallestIntegerFinder/Main.java
@@ -0,0 +1,12 @@
+package com.steel_eyed.SmallestIntegerFinder;
+
+import com.steel_eyed.General.LoggerSetup;
+import org.apache.log4j.Logger;
+
+public class Main {
+	private static final Logger logger = LoggerSetup.init( Main.class );
+
+	public static void main( String[] args ) {
+		logger.info( "Hello World!" );
+	}
+}
